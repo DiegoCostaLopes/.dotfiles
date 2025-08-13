ba55749bf5099911093c214cd873797817a1b562
Diego Lopes <diego.lopes@outlook.com>
2025-08-13 17:59:56 -0300

    WIP

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,16 +1,17 @@
 return {
-  { 'folke/tokyonight.nvim', priority = 1000 },
-  {
-    'comfysage/evergarden',
-    priority = 1000,
-    opts = {
-      transparent_background = true,
-      variant = 'medium', -- 'hard'|'medium'|'soft'
-      overrides = {}, -- add custom overrides
-    },
-  },
-  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = {} },
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-  { 'rebelot/kanagawa.nvim', priority = 1000 },
-  { 'EdenEast/nightfox.nvim' },
+	{ "folke/tokyonight.nvim", priority = 1000 },
+	{
+		"comfysage/evergarden",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+			variant = "medium", -- 'hard'|'medium'|'soft'
+			overrides = {}, -- add custom overrides
+		},
+	},
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = {} },
+	{ "sainnhe/gruvbox-material", priority = 1000, config = true, opts = {} },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "rebelot/kanagawa.nvim", priority = 1000 },
+	{ "EdenEast/nightfox.nvim" },
 }
