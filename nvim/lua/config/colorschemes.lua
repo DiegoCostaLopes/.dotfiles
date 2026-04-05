7bc469af650621ab9bd8c22b18cd544ff9206931
Diego Lopes <diego.lopes@outlook.com>
2026-04-05 11:19:05 -0300

    WIP Noctalia

--- nvim/lua/config/colorschemes.lua
+++ nvim/lua/config/colorschemes.lua
@@ -1,3 +1,5 @@
 vim.pack.add({ "https://github.com/catppuccin/nvim" })
+vim.pack.add({ "https://github.com/sainnhe/gruvbox-material" })
+vim.pack.add({ "https://github.com/rebelot/kanagawa.nvim" })
 
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("gruvbox-material")
