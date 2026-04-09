aba53d197816fc4e80f43caa66ea858d130e9ba5
Diego Lopes <diego.lopes@outlook.com>
2026-04-08 21:36:43 -0300

    Add new colorschemes

--- nvim/lua/config/colorschemes.lua
+++ nvim/lua/config/colorschemes.lua
@@ -1,3 +1,20 @@
 vim.pack.add({ "https://github.com/catppuccin/nvim" })
+vim.pack.add({ "https://github.com/sainnhe/gruvbox-material" })
+vim.pack.add({ "https://github.com/sainnhe/everforest" })
+vim.pack.add({ "https://github.com/folke/tokyonight.nvim" })
+vim.pack.add({ "https://github.com/rebelot/kanagawa.nvim" })
+vim.pack.add({ "https://github.com/shaunsingh/nord.nvim" })
 
 vim.cmd.colorscheme("catppuccin")
+
+require("kanagawa").setup({
+    colors = {
+        theme = {
+            all = {
+                ui = {
+                    bg_gutter = "none",
+                },
+            },
+        },
+    },
+})
